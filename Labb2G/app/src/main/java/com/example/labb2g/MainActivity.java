981fba588d2d98c2d79e40bdb50c23149006a9a0
Pedersen87 <p_atrik@hotmail.com>
2020-09-18 09:52:03 +0200

    Färdig

--- Labb2G/app/src/main/java/com/example/labb2g/MainActivity.java
+++ Labb2G/app/src/main/java/com/example/labb2g/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,71 +19,108 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> words = new ArrayList<>();
+    TextView lifePoints;
     EditText randWord, lInput;
-    String word;
-    Button testButton, letterButton;
-    String underscore, newWord;
-    String newUnderscore = "";
-    //char [] dash;
+    String word, underscore;
+    Button wordButton, letterButton, restartButton;
+    int counter = 7;
+    boolean check = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        words.add("banana");
-        words.add("moose");
-        words.add("motorcycle");
-        words.add("computer");
-        words.add("purple");
+
+        wordlibraryMethod();
         randWord = (EditText) findViewById(R.id.randomWord);
-        testButton = (Button) findViewById(R.id.button);
+        wordButton = (Button) findViewById(R.id.button);
         lInput = (EditText) findViewById(R.id.letterInput);
         letterButton = (Button) findViewById(R.id.checkLetterButton);
+        lifePoints = (TextView) findViewById(R.id.lifePoints);
+        restartButton = (Button) findViewById(R.id.restartButton);
+        letterButton.setVisibility(View.INVISIBLE);
 
-        testButton.setOnClickListener(new View.OnClickListener() {
+        restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Collections.shuffle(words);
-                word = words.get(0);
-                words.remove(0);
-                //underscore = new String(new char[word.length()]).replace("\0", "*"); //TEst
-                underscore = word.replaceAll(".", "*");
+               restartMethod();
+            }
+        });
 
-                randWord.setText(underscore);
-                Toast.makeText(getApplicationContext(), word, Toast.LENGTH_SHORT)
-                        .show();
+        wordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generatewordMethod();
             }
         });
 
         letterButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                for (int i = 0; i < word.length(); i++) {
-                char dash[] = underscore.toCharArray();
-
-                   if (word.charAt(i) == lInput.getText().charAt(0)) {
-                        dash[i] = lInput.getText().charAt(0);
-                        underscore = String.valueOf(dash);
-                       //newUnderscore += lInput.getText().charAt(0);
-                        randWord.setText(underscore);
-                    }
-                    /*if (underscore.charAt(i) != '*') {
-                        newUnderscore += word.charAt(i);
-                    } else {
-                        newUnderscore += "*";
-                    }*/
+                if(lInput.getText().toString().length() != 0){
+                    inputMethod();
                 }
-                if(!underscore.contains("*")){
-                        Toast.makeText(getApplicationContext(), "Vinst", Toast.LENGTH_LONG).show();
-                }
-
-
             }
         });
 
 
-
     }
+        public void restartMethod(){
+            randWord.setText("");
+            lInput.setText("");
+            lifePoints.setText("7");
+            wordButton.setVisibility(View.VISIBLE);
+            letterButton.setVisibility(View.INVISIBLE);
+            counter = 7;
+        }
+
+        public void generatewordMethod(){
+            Collections.shuffle(words);
+            word = words.get(0);
+            //words.remove(0);
+            underscore = word.replaceAll(".", "_");
+            randWord.setText(underscore);
+            letterButton.setVisibility(View.VISIBLE);
+        }
+
+        public void inputMethod(){
+            for (int i = 0; i < word.length(); i++) {
+                char dash[] = underscore.toCharArray();
+                if (word.charAt(i) == lInput.getText().charAt(0)) {
+                    dash[i] = lInput.getText().charAt(0);
+                    underscore = String.valueOf(dash);
+                    randWord.setText(underscore);
+                    check = true;
+                }
+            }
+
+            if(check == false){
+                counter --;
+                lifePoints.setText(String.valueOf(counter));
+            }
+            if(!underscore.contains("_")){
+                Toast.makeText(getApplicationContext(), "You are a winner!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "To start a new game, press restart!", Toast.LENGTH_LONG).show();
+                wordButton.setVisibility(View.INVISIBLE);
+                letterButton.setVisibility(View.INVISIBLE);
+
+            }
+            if(counter <= 0){
+                Toast.makeText(getApplicationContext(), "Looser!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "To start a new game, press restart!", Toast.LENGTH_LONG).show();
+                wordButton.setVisibility(View.INVISIBLE);
+                letterButton.setVisibility(View.INVISIBLE);
+            }
+            check = false;
+        }
+
+        public void wordlibraryMethod(){
+            words.add("monkey");
+            words.add("banana");
+            words.add("moose");
+            words.add("motorcycle");
+            words.add("computer");
+            words.add("purple");
+        }
 }
