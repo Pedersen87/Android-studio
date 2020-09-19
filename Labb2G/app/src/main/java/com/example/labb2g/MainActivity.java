0b3e61864cdae5a9df3371f4df32d940177ad82d
Pedersen87 <p_atrik@hotmail.com>
2020-09-19 16:36:52 +0200

    Labb2VG

--- Labb2G/app/src/main/java/com/example/labb2g/MainActivity.java
+++ Labb2G/app/src/main/java/com/example/labb2g/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,21 +19,24 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> words = new ArrayList<>();
+    //ArrayList<String> words = new ArrayList<>();
     TextView lifePoints;
     EditText randWord, lInput;
     String word, underscore;
+    ImageView hangMan;
     Button wordButton, letterButton, restartButton;
     int counter = 7;
     boolean check = false;
+    Wordlist myWordlist = new Wordlist();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        wordlibraryMethod();
+        Toast.makeText(getApplicationContext(), myWordlist.test, Toast.LENGTH_LONG).show();
+        myWordlist.wordlibraryMethod();
         randWord = (EditText) findViewById(R.id.randomWord);
         wordButton = (Button) findViewById(R.id.button);
         lInput = (EditText) findViewById(R.id.letterInput);
@@ -40,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         lifePoints = (TextView) findViewById(R.id.lifePoints);
         restartButton = (Button) findViewById(R.id.restartButton);
         letterButton.setVisibility(View.INVISIBLE);
+        hangMan = (ImageView) findViewById(R.id.hangMan);
 
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,15 +78,19 @@ public class MainActivity extends AppCompatActivity {
             wordButton.setVisibility(View.VISIBLE);
             letterButton.setVisibility(View.INVISIBLE);
             counter = 7;
+            hangMan.setImageResource(R.drawable.hang);
         }
 
         public void generatewordMethod(){
-            Collections.shuffle(words);
-            word = words.get(0);
-            //words.remove(0);
+            Collections.shuffle(myWordlist.words);
+            word = myWordlist.words.get(0);
+
+           // Collections.shuffle(words);
+           // word = words.get(0);
             underscore = word.replaceAll(".", "_");
             randWord.setText(underscore);
             letterButton.setVisibility(View.VISIBLE);
+
         }
 
         public void inputMethod(){
@@ -98,6 +107,27 @@ public class MainActivity extends AppCompatActivity {
             if(check == false){
                 counter --;
                 lifePoints.setText(String.valueOf(counter));
+                if(counter == 6){
+                    hangMan.setImageResource(R.drawable.hang1);
+                }
+                if(counter == 5){
+                    hangMan.setImageResource(R.drawable.hang2);
+                }
+                if(counter == 4){
+                    hangMan.setImageResource(R.drawable.hang3);
+                }
+                if(counter == 3){
+                    hangMan.setImageResource(R.drawable.hang4);
+                }
+                if(counter == 2){
+                    hangMan.setImageResource(R.drawable.hang5);
+                }
+                if(counter == 1){
+                    hangMan.setImageResource(R.drawable.hang6);
+                }
+                if(counter == 0){
+                    hangMan.setImageResource(R.drawable.hang7);
+                }
             }
             if(!underscore.contains("_")){
                 Toast.makeText(getApplicationContext(), "You are a winner!", Toast.LENGTH_LONG).show();
@@ -115,12 +145,12 @@ public class MainActivity extends AppCompatActivity {
             check = false;
         }
 
-        public void wordlibraryMethod(){
+        /*public void wordlibraryMethod(){
             words.add("monkey");
             words.add("banana");
             words.add("moose");
             words.add("motorcycle");
             words.add("computer");
             words.add("purple");
-        }
+        }*/
 }
---
+++ Labb2G/app/src/main/java/com/example/labb2g/Wordlist.java
@@ -0,0 +1,21 @@
+package com.example.labb2g;
+
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Wordlist {
+    String test = "WordListclass";
+
+    //Nedanstående test
+    ArrayList<String> words = new ArrayList<>();
+
+    public void wordlibraryMethod(){
+        words.add("monkey");
+        words.add("banana");
+        words.add("moose");
+        words.add("motorcycle");
+        words.add("computer");
+        words.add("purple");
+    }
+}
