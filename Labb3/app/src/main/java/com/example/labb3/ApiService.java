b1a85639cab778ccc3fe44cba8762399767f40c0
Pedersen87 <p_atrik@hotmail.com>
2020-10-14 11:11:55 +0200

    Labb3 backup

--- Labb3/app/src/main/java/com/example/labb3/ApiService.java
+++ Labb3/app/src/main/java/com/example/labb3/ApiService.java
@@ -2,37 +2,79 @@ package com.example.labb3;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class ApiService extends IntentService {
+public class ApiService extends AsyncTask<String, Void, String> {
+    private ArrayList<String> list = new ArrayList<>();
+    private String getSimilar;
+    private String artistInput;
+    MainActivity m = new MainActivity();
 
-    private static final String TAG = "ApiService";
+        public ApiService(String artistInput){
+                this.artistInput = artistInput;
+        }
 
-    public ApiService() {
-        super(TAG);
-    }
+        public void setArtistInput(String artistInput){
+               this.artistInput = artistInput;
+        }
 
-    public ApiService(String name) {
-        super(name);
-    }
+        public String getArtistInput(){
+                return artistInput;
+        }
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        for (int i = 0; i < 5; i++) {
-            Log.d(TAG, "Background service waiting for two seconds loop " + (i + 1));
-            System.out.println("TJOOOOO");
-            synchronized (this) {
-                try {
+        @Override
+        protected String doInBackground(String... strings) {
 
-                    wait(2000);
+            String artistSearch = getArtistInput();
+            list = new ArrayList<>();
+            URL url;
+            for(int i =0; i<1; i++){
+                try{
+                    url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artistSearch+"&limit=5&api_key=7da7759f316358f0712e089cc08bd518");
+                    Log.d("Eservice", "Calling URL: "+url.toString());
+                    XmlPullParserFactory parserCreator = XmlPullParserFactory
+                            .newInstance();
+                    XmlPullParser parser = parserCreator.newPullParser();
+                    parser.setInput(url.openStream(), null);
+                    int parserEvent = parser.getEventType();
+                    String tagName;
+                    while (parserEvent != XmlPullParser.END_DOCUMENT) {
+                        if (parserEvent==XmlPullParser.START_TAG) {
+                            tagName = parser.getName();
+                            Log.d("Eservice", "Start tag found: "+tagName);
 
+                            if(tagName.equals("name")){
+                                parser.next();
+                                getSimilar = parser.getText();
+                                list.add(getSimilar);
+                            }
+                        }
+                        parserEvent = parser.next();
+                    }
                 } catch (Exception e) {
-
                     e.printStackTrace();
-
                 }
             }
+            return null;
         }
+    @Override
+    protected void onPostExecute(String s) {
+        System.out.println("Liknande artister: " + Arrays.toString(list.toArray()));
+
+        /* listAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, list);
+        ListView listView = (ListView) (findViewById(R.id.listArtist));
+        listView.setAdapter(listAdapter);*/
+        super.onPostExecute(s);
     }
-}
+    }
+
--- Labb3/app/src/main/java/com/example/labb3/MainActivity.java
+++ Labb3/app/src/main/java/com/example/labb3/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.labb3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,10 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
-
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,6 +26,7 @@ public class MainActivity extends AppCompatActivity  {
     private String getSimilar;
     ProgressDialog pD;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,72 +37,10 @@ public class MainActivity extends AppCompatActivity  {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               AsyncTaskApi ata = new AsyncTaskApi();
-               ata.execute();
+                ApiService as = new ApiService(artistInput.getText().toString());
+                as.setArtistInput(artistInput.getText().toString());
+                as.execute();
             }
         });
     }
-            private class AsyncTaskApi extends AsyncTask<Void, Void, String>{
-
-                /*@Override
-                protected void onPreExecute(){
-                    pD = new ProgressDialog(MainActivity.this);
-                    pD.setMessage("Loading...");
-                    pD.setIndeterminate(false);
-                    pD.setCancelable(false);
-                    pD.show();
-                }*/
-
-                @Override
-                protected String doInBackground(Void... voids) {
-                    String artistSearch = artistInput.getText().toString();
-                    list = new ArrayList<>();
-                    URL url;
-                    for(int i =0; i<1; i++){
-                        try{
-                            url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artistSearch+"&limit=5&api_key=7da7759f316358f0712e089cc08bd518");
-                            Log.d("Eservice", "Calling URL: "+url.toString());
-                            XmlPullParserFactory parserCreator = XmlPullParserFactory
-                                    .newInstance();
-                            XmlPullParser parser = parserCreator.newPullParser();
-                            parser.setInput(url.openStream(), null);
-                            int parserEvent = parser.getEventType();
-                            String tagName;
-                            while (parserEvent != XmlPullParser.END_DOCUMENT) {
-                                if (parserEvent==XmlPullParser.START_TAG) {
-                                    tagName = parser.getName();
-                                    Log.d("Eservice", "Start tag found: "+tagName);
-
-                                    if(tagName.equals("name")){
-                                        parser.next();
-                                        getSimilar = parser.getText();
-                                        list.add(getSimilar);
-                                    }
-                                } //Eventuellt nedanför nedanstående rad
-                                    parserEvent = parser.next();
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(String s) {
-                    listAdapter = new ArrayAdapter<String>(MainActivity.this,
-                    android.R.layout.simple_list_item_1, list);
-                    ListView listView = (ListView) (findViewById(R.id.listArtist));
-                    listView.setAdapter(listAdapter);
-                    super.onPostExecute(s);
-                }
-            }
-
-
-
-
-
-
-
-
 }
